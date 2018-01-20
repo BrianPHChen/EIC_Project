5e167089c36be27fa2dcb37054198ba0eee327fe
brianPHChen <brian770130@gmail.com>
2018-01-20 18:18:49 +0800

    Add ratio

--- contracts/CrowdSales.sol
+++ contracts/CrowdSales.sol
@@ -9,10 +9,15 @@ contract CrowdSales {
 
 	EICToken public token;
 
-    uint public startBlock;
-	uint public endBlock;
     uint public receivedWei;
     uint public tokenPrice;
+
+    struct Beneficiary {
+        address addr;
+        uint256 ratio;
+    }
+
+    Beneficiary[] public beneficiaries;
 
     event Bid(address indexed bider, uint256 getToken);
 
@@ -23,12 +28,17 @@ contract CrowdSales {
 
     function CrowdSales() public {
         owner = msg.sender;
+        beneficiaries.push(Beneficiary(0xA5A6b44312a2fc363D78A5af22a561E9BD3151be, 10));
+        beneficiaries.push(Beneficiary(0x8Ec21f2f285545BEc0208876FAd153e0DEE581Ba, 10));
+        beneficiaries.push(Beneficiary(0x81D98B74Be1C612047fEcED3c316357c48daDc83, 5));
+        beneficiaries.push(Beneficiary(0x882Efb2c4F3B572e3A8B33eb668eeEdF1e88e7f0, 10));
+        beneficiaries.push(Beneficiary(0xe63286CCaB12E10B9AB01bd191F83d2262bde078, 15));
+        beneficiaries.push(Beneficiary(0xCcab73497D432a07705DCca58358e00F87bA4CD5, 285));
+        beneficiaries.push(Beneficiary(0x4583408F92427C52D1E45500Ab402107972b2CA6, 665));
     }
 
-    function setUP(address _tokenAddress, uint blockPeriod) public onlyOwner {
+    function setUP(address _tokenAddress) public onlyOwner {
     	token = EICToken(_tokenAddress);
-    	startBlock = block.number;
-    	endBlock = startBlock + blockPeriod;
     	tokenPrice = 15000;
     }
 
@@ -40,8 +50,9 @@ contract CrowdSales {
     	public
     	payable
     {
-    	require(startBlock != 0);
-    	require(block.number <= endBlock);
+    	require(tokenPrice != 0);
+    	require(block.number <= token.lockBlock());
+        require(receivedWei <= 62500 * ( 10 ** 18 ));
     	require(token.balanceOf(msg.sender).add(msg.value.mul(tokenPrice)) >= uint256(5 * (10 ** 18)).mul(tokenPrice));
     	require(token.balanceOf(msg.sender).add(msg.value.mul(tokenPrice)) <= uint256(200 * (10 ** 18)).mul(tokenPrice));
         token.transfer(msg.sender, msg.value.mul(tokenPrice));
@@ -50,6 +61,12 @@ contract CrowdSales {
     }
 
     function finalize() onlyOwner {
-    	require(block.number > endBlock);
+    	require(block.number > token.lockBlock() || receivedWei == 62500 * ( 10 ** 18 ));
+        for (uint i = 0; i < beneficiaries.length; i++) {
+            Beneficiary storage beneficiary = beneficiaries[i];
+            uint256 value = (receivedWei.mul(beneficiary.ratio)).div(1000);
+            beneficiary.addr.transfer(value);
+        }
+        owner.transfer(this.balance);
     }
 }
--- contracts/PrivateSales.sol
+++ contracts/PrivateSales.sol
@@ -9,10 +9,15 @@ contract PrivateSales {
 
 	EICToken public token;
 
-    uint public startBlock;
-	uint public endBlock;
     uint public receivedWei;
     uint public tokenPrice;
+
+    struct Beneficiary {
+        address addr;
+        uint ratio;
+    }
+
+    Beneficiary[] public beneficiaries;
 
     event Bid(address indexed bider, uint256 getToken);
 
@@ -23,12 +28,16 @@ contract PrivateSales {
 
     function PrivateSales() public {
         owner = msg.sender;
+        beneficiaries.push(Beneficiary(0xA5A6b44312a2fc363D78A5af22a561E9BD3151be, 10));
+        beneficiaries.push(Beneficiary(0x8Ec21f2f285545BEc0208876FAd153e0DEE581Ba, 10));
+        beneficiaries.push(Beneficiary(0x81D98B74Be1C612047fEcED3c316357c48daDc83, 5));
+        beneficiaries.push(Beneficiary(0x882Efb2c4F3B572e3A8B33eb668eeEdF1e88e7f0, 10));
+        beneficiaries.push(Beneficiary(0xe63286CCaB12E10B9AB01bd191F83d2262bde078, 15));
+        beneficiaries.push(Beneficiary(0x4583408F92427C52D1E45500Ab402107972b2CA6, 950));
     }
 
-    function setUP(address _tokenAddress, uint blockPeriod) public onlyOwner {
+    function setUP(address _tokenAddress) public onlyOwner {
     	token = EICToken(_tokenAddress);
-    	startBlock = block.number;
-    	endBlock = startBlock + blockPeriod;
     	tokenPrice = 15000;
     }
 
@@ -46,6 +55,12 @@ contract PrivateSales {
     }
 
     function finalize() public onlyOwner {
-
+        require(receivedWei == 31250 * ( 10 ** 18 ));
+        for (uint i = 0; i < beneficiaries.length; i++) {
+            Beneficiary beneficiary = beneficiaries[i];
+            uint256 value = (receivedWei.mul(beneficiary.ratio)).div(1000);
+            beneficiary.addr.transfer(value);
+        }
+        owner.transfer(this.balance);
     }
 }
