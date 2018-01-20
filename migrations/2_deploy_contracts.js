6e0a2880a4c41938d144c78eef26005ad3116164
Ben Lu <ben.lu@isuncloud.com>
2018-01-20 22:13:09 +0800

    Link contracts.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,8 @@ var PrivateSales = artifacts.require("./PrivateSales.sol");
 var lockBlockPeriod = 0;
 
 module.exports = function(deployer) {
-	deployer.deploy(EICToken, lockBlockPeriod);
- 	deployer.deploy(CrowdSales);
-    deployer.deploy(PrivateSales);
+	deployer.deploy(EICToken, lockBlockPeriod).then(() => {
+		deployer.deploy(CrowdSales, EICToken.address);
+		return deployer.deploy(PrivateSales, EICToken.address);
+	});
 };
