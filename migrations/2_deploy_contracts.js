b016610ca54c2f48b90ac34deb08b3d22091ffd4
Ben Lu <ben.lu@isuncloud.com>
2018-01-21 16:05:51 +0800

    Change lock to 200 blocks.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,11 +2,11 @@ var EICToken = artifacts.require("./EICToken.sol");
 var CrowdSales = artifacts.require("./CrowdSales.sol");
 var PrivateSales = artifacts.require("./PrivateSales.sol");
 
-var lockBlockPeriod = 0;
+var lockBlockPeriod = 200;
 
 module.exports = function(deployer) {
 	deployer.deploy(EICToken, lockBlockPeriod).then(() => {
 		deployer.deploy(CrowdSales, EICToken.address);
-		return deployer.deploy(PrivateSales, EICToken.address);
+		deployer.deploy(PrivateSales, EICToken.address);
 	});
 };
